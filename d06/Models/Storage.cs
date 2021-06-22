bcfec7a4a8cfd5cef115d43759f38bb7f21fde6e
alkuramshina <alkuramshina@gmail.com>
2021-06-22 17:55:56 +0300

    Add files via upload

---
+++ d06/Models/Storage.cs
@@ -0,0 +1,13 @@
+﻿namespace d06.Models
+{
+    public class Storage
+    {
+        public int ItemsInStorage { get; set; }
+        public bool IsEmpty => ItemsInStorage <= 0;
+        
+        public Storage(int totalItemCount)
+        {
+            ItemsInStorage = totalItemCount;
+        }
+    }
+}
---
+++ d06/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using d06.Extensions;
+using d06.Models;
+
+namespace d06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int registerCount = 3;
+            const int storageCapacity = 40;
+            const int cartCapacity = 7;
+            const int customerCount = 10;
+
+            var customers = Enumerable.Range(1, customerCount)
+                .Select(x => new Customer(x))
+                .ToArray();
+            
+            var shop = new Store(registerCount,
+                storageCapacity);
+            
+            Console.WriteLine("Lines by people count:");
+
+            var i = 0;
+            while (shop.IsOpen)
+            {
+                var customer = customers[i++];
+                
+                customer.FillCart(cartCapacity);
+                shop.Storage.ItemsInStorage -= customer.ItemsInCart;
+                
+                var register = customer.GetInLineByPeople(shop.Registers);
+                Console.WriteLine($"{customer} to {register}");
+            }
+        }
+    }
+}
